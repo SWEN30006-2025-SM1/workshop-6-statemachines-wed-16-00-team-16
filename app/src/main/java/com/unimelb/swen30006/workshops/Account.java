f42066f1351bfefedc1a28ffded8d7fde45a22bb
dylan tran <dytran@student.unimelb.edu.au>
2025-04-16 17:36:32 +1000

    Account Class Done

--- app/src/main/java/com/unimelb/swen30006/workshops/Account.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Account.java
@@ -1,5 +1,12 @@
 package com.unimelb.swen30006.workshops;
 
 public class Account {
+    private int accountID;
+
     public void onboard() {}
+    public void activateCard() {}
+    public void reportLostorFound() {}
+    public void exceedLimit() {}
+    public void resumeAccount() {}
+    public void inactivityTimeout() {}
 }
